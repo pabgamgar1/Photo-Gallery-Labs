89885abd1461a45ad190cf2c653ca9730464dc0c
pabgamgar <blabligamerito@gmail.com>
2025-04-09 10:56:24 +0200

    Porject restart

--- settings.py
+++ settings.py
@@ -9,8 +9,8 @@ DEBUG_ENABLED = False
 DB_CONN = {
     "host": "127.0.0.1",
     "port": 3306,
-    "username": "root",
-    "password": "iissi$root",
+    "username": "iissi_user",
+    "password": "iissi$user",
     "database": "gallery",
 }
 
